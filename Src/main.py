4f3e04174f89511b86eb4806d808049c47b441ae
Deja-Vu1 <ilkanseckin@gmail.com>
2021-04-08 23:34:52 +0300

    Mobile configuration

--- Src/main.py
+++ Src/main.py
@@ -4,12 +4,14 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions
 from discord.ext.commands import CommandNotFound
 from discord.ext import tasks
+from discord.ext import menus
 import asyncio
 import requests
 import json
 import pytz
 from pytz import timezone
 import os
+
 
 dictofdays = {"Thursday":"Perşembe","Wednesday":"Çarşamba","Tuesday":"Salı","Monday":"Pazartesi","Sunday":"Pazar","Saturday":"Cumartesi","Friday":"Cuma"}
 ADMINID = 596455467585110016
@@ -24,6 +26,10 @@ async def on_ready():
 	print('------');await bot.change_presence(
         activity=discord.Game(name="!weather city | 🌐 " + str(len(bot.guilds))+" servers")
         )
+
+class MultiPageEmbed(menus.ListPageSource):
+    async def format_page(self, menu, entry):
+        return entry
 
 @bot.command()
 async def weather(ctx,*args):
@@ -46,8 +52,6 @@ async def weather(ctx,*args):
     if a["cod"] == 200 and a["list"] != []:
         b = requests.post(f'https://openweathermap.org/data/2.5/onecall?lat={a["list"][0]["coord"]["lat"]}&lon={a["list"][0]["coord"]["lon"]}&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02')
         b = json.loads(b.text)
-
-        # useful variables
         des = a["list"][0]["weather"][0]["description"]
         arr = a["list"][0]["weather"][0]["main"]
         deg = a["list"][0]["main"]["temp"] - 273.15
@@ -58,7 +62,6 @@ async def weather(ctx,*args):
         hum = a["list"][0]["main"]["humidity"]
         if a["list"][0]["rain"] != None:footext+=" 🌧"
         if a["list"][0]["snow"] != None:footext+=" ❄️"
-        # -------------------
 
         utc_now = datetime.datetime.utcnow()
         utc = pytz.timezone('UTC')
@@ -76,11 +79,16 @@ async def weather(ctx,*args):
         embed.add_field(name=f"Hissedilen : **{round(feel, 5)}**°C", value="‎", inline=True)
         embed.add_field(name=f"💨 Basınç : **{press}**hPa", value="‎",inline=True)
         embed.add_field(name=f"Nem : **{round(hum, 5)}**%", value="‎‎", inline=True)
+
+        embed2 = discord.Embed(title=name.upper(), colour=discord.Colour(0x9beec), url="https://discordapp.com") 
+
+        embed2.set_thumbnail(url=f'http://openweathermap.org/img/wn/{a["list"][0]["weather"][0]["icon"]}.png')
+        embed2.set_author(name=ctx.author.name, url="https://discordapp.com", icon_url=ctx.author.avatar_url)
+        embed2.set_footer(text=footext+" • bugün saat "+str(now_turkey.hour)+":"+str(now_turkey.minute), icon_url="https://cdn.discordapp.com/avatars/596455467585110016/b96ac044a4382f62ad36637c6021ef80.png?size=256")
+
         daily=""
         days=[]
-
-        # formatting for last 7 days
-        for i in range(0,7):
+        for i in range(1,8):
             dt = str(b["daily"][i]["dt"])
             deg = str(round(b["daily"][i]["temp"]["day"],2))
             arr = str(b["daily"][i]["weather"][0]["main"])
@@ -88,28 +96,50 @@ async def weather(ctx,*args):
             hum = str(round(b["daily"][i]["humidity"],2))
             day = dictofdays[str(datetime.datetime.fromtimestamp(int(dt)).strftime("%A"))]
             days.append([day,deg,arr,press,hum])
-        # -------------------
             
-        day2day = f"geçen {days[0][0]} ──► geçen {days[6][0]}"
-        template = f"""{day2day.center(44, " ")}
-┌─────────┐┌─────────┐┌─────────┐┌─────────┐
-│{days[0][0].center(9, " ")}││{days[1][0].center(9, " ")}││{days[2][0].center(9, " ")}││{days[3][0].center(9, " ")}│
-├─────────┤├─────────┤├─────────┤├─────────┤
-│°C:{days[0][1].rjust(6, " ")}││°C:{days[1][1].rjust(6, " ")}││°C:{days[2][1].rjust(6, " ")}││°C:{days[3][1].rjust(6, " ")}│
-│{days[0][2].center(9, " ")}││{days[1][2].center(9, " ")}││{days[2][2].center(9, " ")}││{days[3][2].center(9, " ")}│
-│hPa:{days[0][3].rjust(5, " ")}││hPa:{days[1][3].rjust(5, " ")}││hPa:{days[2][3].rjust(5, " ")}││hPa:{days[3][3].rjust(5, " ")}│
-│Nem:{days[0][4].rjust(5, " ")}││Nem:{days[1][4].rjust(5, " ")}││Nem:{days[2][4].rjust(5, " ")}││Nem:{days[3][4].rjust(5, " ")}│
-└─────────┘└─────────┘└─────────┘└─────────┘
-     ┌─────────┐┌─────────┐┌─────────┐ 
-     │{days[4][0].center(9, " ")}││{days[5][0].center(9, " ")}││{days[6][0].center(9, " ")}│
-     ├─────────┤├─────────┤├─────────┤
-     │°C:{days[4][1].rjust(6, " ")}││°C:{days[5][1].rjust(6, " ")}││°C:{days[6][1].rjust(6, " ")}│
-     │{days[4][2].center(9, " ")}││{days[5][2].center(9, " ")}││{days[6][2].center(9, " ")}│
-     │hPa:{days[4][3].rjust(5, " ")}││hPa:{days[5][3].rjust(5, " ")}││hPa:{days[6][3].rjust(5, " ")}│
-     │Nem:{days[4][4].rjust(5, " ")}││Nem:{days[5][4].rjust(5, " ")}││Nem:{days[6][4].rjust(5, " ")}│
-     └─────────┘└─────────┘└─────────┘"""
+        day2day = f"önümüzdeki {days[0][0]},{days[1][0]}"
+        template = f"""{day2day}
+┌─────────┐┌─────────┐
+│{days[0][0].center(9, " ")}││{days[1][0].center(9, " ")}│
+├─────────┤├─────────┤
+│°C:{days[0][1].rjust(6, " ")}││°C:{days[1][1].rjust(6, " ")}│
+│{days[0][2].center(9, " ")}││{days[1][2].center(9, " ")}│
+│hPa:{days[0][3].rjust(5, " ")}││hPa:{days[1][3].rjust(5, " ")}│
+│Nem:{days[0][4].rjust(5, " ")}││Nem:{days[1][4].rjust(5, " ")}│
+└─────────┘└─────────┘"""
         embed.add_field(name="‎‎", value=f"```{template}```",inline=False)
-        await ctx.send(embed=embed)
+
+        day2day = f"önümüzdeki {days[2][0]} ──► önümüzdeki {days[6][0]}"
+        template = f"""{day2day}
+┌─────────┐┌─────────┐
+│{days[2][0].center(9, " ")}││{days[3][0].center(9, " ")}│
+├─────────┤├─────────┤
+│°C:{days[2][1].rjust(6, " ")}││°C:{days[3][1].rjust(6, " ")}│
+│{days[2][2].center(9, " ")}││{days[3][2].center(9, " ")}│
+│hPa:{days[2][3].rjust(5, " ")}││hPa:{days[3][3].rjust(5, " ")}│
+│Nem:{days[2][4].rjust(5, " ")}││Nem:{days[3][4].rjust(5, " ")}│
+└─────────┘└─────────┘
+┌─────────┐┌─────────┐
+│{days[4][0].center(9, " ")}││{days[5][0].center(9, " ")}│
+├─────────┤├─────────┤ 
+│°C:{days[4][1].rjust(6, " ")}││°C:{days[5][1].rjust(6, " ")}│
+│{days[4][2].center(9, " ")}││{days[5][2].center(9, " ")}│
+│hPa:{days[4][3].rjust(5, " ")}││hPa:{days[5][3].rjust(5, " ")}│
+│Nem:{days[4][4].rjust(5, " ")}││Nem:{days[5][4].rjust(5, " ")}│
+└─────────┘└─────────┘
+     ┌─────────┐
+     │{days[6][0].center(9, " ")}│
+     ├─────────┤
+     │°C:{days[6][1].rjust(6, " ")}│
+     │{days[6][2].center(9, " ")}│
+     │hPa:{days[6][3].rjust(5, " ")}│
+     │Nem:{days[6][4].rjust(5, " ")}│
+     └─────────┘"""
+        embed2.add_field(name="‎‎", value=f"```{template}```",inline=False)
+
+        embeds = [embed,embed2]
+        menu = menus.MenuPages(MultiPageEmbed(embeds, per_page=1))
+        await menu.start(ctx)
     else:
         embed = discord.Embed(title="Pardon bulamadım :/", colour=discord.Colour(0x9beec), timestamp=datetime.datetime.utcfromtimestamp(1617872085))
 
@@ -145,5 +175,5 @@ async def on_guild_remove(guild):
     await bot.change_presence(
         activity=discord.Game(
             name="!weather city | 🌐 " + str(len(bot.guilds)) + " servers"))
-
+            
 bot.run(os.getenv("TOKEN"))
